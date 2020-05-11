8daed7ed5dcbdbc093be5387d79321fda8d12ad3
Albert Cervantes <cervanalbert@gmail.com>
2020-05-10 21:28:06 -0700

    Updates

---
+++ precache-manifest.c1e37be1925a9ea7fa5b83f1a178ec32.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57a1ee7f38e16d9f670803835acc3a6d",
+    "url": "/user-directory-react-table/index.html"
+  },
+  {
+    "revision": "c4e6fa21aaf83b05fd3f",
+    "url": "/user-directory-react-table/static/css/2.8fe25b83.chunk.css"
+  },
+  {
+    "revision": "c4e6fa21aaf83b05fd3f",
+    "url": "/user-directory-react-table/static/js/2.5687369a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/user-directory-react-table/static/js/2.5687369a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbf6cea9abed8e644f26",
+    "url": "/user-directory-react-table/static/js/main.763cd40a.chunk.js"
+  },
+  {
+    "revision": "be9979e1d29f089d58f2",
+    "url": "/user-directory-react-table/static/js/runtime-main.3adb72da.js"
+  }
+]);
